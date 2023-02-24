65a76144638848a2d24d5ae2d28528aa12358137
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-02-24 23:47:23 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def requirements():
 
 setuptools.setup(
 	name='flask-supporter',
-	version='0.0.10',
+	version='0.0.11',
 	description='Flask supporter',
 	long_description=open('README.md').read(),
 	long_description_content_type='text/markdown',
