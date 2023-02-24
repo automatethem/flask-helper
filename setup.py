36d1d883193588f3210b14fc67fb73b2ba71dc0e
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-02-24 15:36:06 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def requirements():
 setuptools.setup(
 	name='flask-supporter',
 	version='0.0.2',
-	description='Flask supporter',
+	description='Flask supporter.',
 	long_description=open('README.md').read(),
 	author='Sang Ki Kwon',
 	url='https://github.com/automatethem/flask-supporter',
