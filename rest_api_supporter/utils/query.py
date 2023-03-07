cff0d24772e0cf7b303788dbfe53469b335d1813
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-07 11:41:43 +0900

    Update query.py

--- rest_api_supporter/utils/query.py
+++ rest_api_supporter/utils/query.py
@@ -3,4 +3,4 @@ import requests
 def query(url, json=None, data=None, headers=None, timeout=60):
     response = requests.post(url, json=json, data=data, headers=headers, timeout=timeout)
     outputs = response.json()
-    retirm outputs
+    return outputs
