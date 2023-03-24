db1215536a273e47abac819a6d01e7048917d6dd
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-24 18:02:05 +0900

    Update predict.py

--- examples/predict.py
+++ examples/predict.py
@@ -1,10 +1,12 @@
 import rest_api_supporter
 
-url = 'https://automatethem.pythonanywhere.com/son-height-tabular-regression-scikit-learn/api'
+#url = 'https://automatethem.pythonanywhere.com:8000/apis/son-height-tabular-regression-api'
+url = 'http://localhost:8000/apis/son-height-tabular-regression-api'
 
-user_input = 175
-payload = {'data': [{'x': str(user_input)}]}
+payload = {'data': [{'x': 175}]}
+#print(payload) #{'data': [{'x': 175}]}
 outputs = rest_api_supporter.utils.query(url, json=payload)
+#outputs = {'data': [{'logit': 175.99442286524624}]} #query(url, json=payload)
 #print(outputs) #{'data': [{'logit': 175.99442286524624}]}
 result = outputs['data'][0]['logit']
-print(result)
+print(result) #175.99442286524624
