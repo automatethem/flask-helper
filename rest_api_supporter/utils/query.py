aff87e7be607e9ad092a392cb2bd8b7e6f8a3b47
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-04-27 20:53:35 +0900

    Delete query.py

--- rest_api_supporter/utils/query.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def query(url, json=None, data=None, headers=None, timeout=60):
-    response = requests.post(url, json=json, data=data, headers=headers, timeout=timeout)
-    outputs = response.json()
-    return outputs
