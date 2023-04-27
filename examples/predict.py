874f5118397e69e3b721c569b4916be3cacbd4d5
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-04-27 20:54:33 +0900

    Update predict.py

--- examples/predict.py
+++ examples/predict.py
@@ -5,8 +5,8 @@ url = 'http://localhost:8000/apis/son-height-tabular-regression-api'
 
 payload = {'data': [{'x': 175}]}
 #print(payload) #{'data': [{'x': 175}]}
-outputs = rest_api_supporter.utils.query(url, json=payload)
-#outputs = {'data': [{'logit': 175.99442286524624}]} #query(url, json=payload)
+outputs = rest_api_supporter.utils.post(url, json=payload)
+#outputs = {'data': [{'logit': 175.99442286524624}]} 
 #print(outputs) #{'data': [{'logit': 175.99442286524624}]}
 result = outputs['data'][0]['logit']
 print(result) #175.99442286524624
