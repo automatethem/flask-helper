6af64794e1951019281e3a74d3aad3cdc8a9efdc
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-04-27 20:52:11 +0900

    Update __init__.py

--- rest_api_supporter/utils/__init__.py
+++ rest_api_supporter/utils/__init__.py
@@ -1,9 +1,7 @@
 from . import image_to_base64
-#from . import query
 from . import post
 from . import get
 
 from .image_to_base64 import image_to_base64
-#from .query import query
 from .post import post
 from .get import get
