18b043fcb1608cb5ae5047b21218d1cb21aa4d4f
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-04-27 18:07:04 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def requirements():
 
 setuptools.setup(
 	name='rest-api-supporter',
-	version='0.0.5',
+	version='0.0.6',
 	description='Rest api supporter',
 	long_description=open('README.md').read(),
 	long_description_content_type='text/markdown',
