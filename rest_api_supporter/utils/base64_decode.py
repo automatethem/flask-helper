764538b6c500d2c97b25220bb8a73ebdbdcdadb4
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-27 12:24:07 +0900

    Update base64_decode.py

--- rest_api_supporter/utils/base64_decode.py
+++ rest_api_supporter/utils/base64_decode.py
@@ -1,20 +1,31 @@
 import base64
+import io
 from PIL import Image
-import re
 
-def base64_decode(base64):
-    #print(base64) #data:image/jpeg;base64,/9j/4TT...
-    if "base64," in base64:
-        front = base64.split('base64,')[0]
-        encoded = base64.split('base64,')[1]
+'''
+#print(full_encoded) #data:image/png;base64,/9j/4AAQSkZJRgABAQ...2qjR37P/2Q==
+                     #data:audio/wav;base64,UklGRiTuAgBXQVZFZm...At84WACNZGwA=
+                     #/9j/4AAQSkZJRgABAQ...2qjR37P/2Q==
+base64_decoded = base64_decode(full_encoded)
+print(type(base64_decoded)) #<class 'PIL.JpegImagePlugin.JpegImageFile'>
+                            #<class 'bytes'>
+                            #<class 'PIL.JpegImagePlugin.JpegImageFile'>
+'''
+def base64_decode(full_encoded):
+    if "base64," in full_encoded:
+        #print(full_encoded) #data:image/png;base64,/9j/4AAQSkZJRgABAQ...2qjR37P/2Q==
+                             #data:audio/wav;base64,UklGRiTuAgBXQVZFZm...At84WACNZGwA=
+        front = full_encoded.split('base64,')[0]
+        encoded = full_encoded.split('base64,')[1]
         decoded = base64.b64decode(encoded)
         if "image" in front:
-            image = Image.open(decoded)
-            #print(image) #<PIL.PngImagePlugin.PngImageFile image mode=RGBA size=386x262 at 0x1074728F0>
+            image = Image.open(io.BytesIO(decoded))
             return image
         elif "audio" in front:
             return decoded
     else:
-        image = Image.open(decoded)
-        #print(image) #<PIL.PngImagePlugin.PngImageFile image mode=RGBA size=386x262 at 0x1074728F0>
+        #print(full_encoded) #/9j/4AAQSkZJRgABAQ...2qjR37P/2Q==
+                             #UklGRiTuAgBXQVZFZm...At84WACNZGwA=
+        decoded = base64.b64decode(full_encoded)
+        image = Image.open(io.BytesIO(decoded))
         return image
