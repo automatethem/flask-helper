94aaca07c883f516ced6a3c530ae045e9258a8eb
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-30 07:20:28 +0900

    Update base64_encode.py

--- rest_api_supporter/utils/base64_encode.py
+++ rest_api_supporter/utils/base64_encode.py
@@ -2,6 +2,7 @@ import io
 import base64
 from PIL import Image
 
+'''
 #image = Image.open("rock.jpg")
 #base64 = base64_encode(image)
 #print(base64) #/9j/4AAQSkZJRgABAQAAA ... bSjrTEf/9k=
@@ -14,3 +15,22 @@ def base64_encode(image):
     b64encoded = base64.b64encode(bytesIO.getvalue())
     base64_str = b64encoded.decode("utf-8")
     return base64_str
+'''
+
+#image = Image.open("rock.jpg")
+#base64 = base64_encode(image)
+#print(base64) #/9j/4AAQSkZJRgABAQAAA ... bSjrTEf/9k=
+def base64_encode(bytes):
+    if isinstance(image, Image):
+        bytesIO = io.BytesIO()
+        try:
+            image.save(bytesIO, "JPEG")
+        except:
+            image.save(bytesIO, "PNG")
+        bytes = bytesIO.getvalue()
+    b64encoded = base64.b64encode(bytes)
+    base64_str = b64encoded.decode("utf-8")
+    if isinstance(image, Image):    
+        return "data:image/png;base64,"+base64_str
+    else:    
+        return "data:audio/wav;base64,"+base64_str
