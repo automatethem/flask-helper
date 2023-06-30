2036a1b6263793794d03e243106ec8aa2fe56b9a
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-01 01:04:27 +0900

    Update base64_postprocess.py

--- rest_api_supporter/utils/base64_postprocess.py
+++ rest_api_supporter/utils/base64_postprocess.py
@@ -9,12 +9,12 @@ def base64_postprocess(prediction):
         for i, p in enumerate(prediction):
             for k in p.keys():
                 v = p[k]
-                if isinstance(v, Image.Image) or isinstance(v, numpy.ndarray):
+                if isinstance(v, Image.Image) or isinstance(v, numpy.ndarray) or isinstance(v, bytes):
                     base64_encoded = base64_encode(v)
                     prediction[i][k] = base64_encoded
     elif isinstance(prediction, dict):   
         for k in prediction.keys():
             v = prediction[k]
-            if isinstance(v, Image.Image) or isinstance(v, numpy.ndarray):
+            if isinstance(v, Image.Image) or isinstance(v, numpy.ndarray) or isinstance(v, bytes):
                 base64_encoded = base64_encode(v)
                 prediction[k] = base64_encoded
