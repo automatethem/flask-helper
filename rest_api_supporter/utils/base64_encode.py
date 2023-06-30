71e69d865c7a7190372979c549f3231b4445deb8
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-30 09:10:40 +0900

    Update base64_encode.py

--- rest_api_supporter/utils/base64_encode.py
+++ rest_api_supporter/utils/base64_encode.py
@@ -1,21 +1,7 @@
 import io
 import base64
 from PIL import Image
-
-'''
-#image = Image.open("rock.jpg")
-#base64 = base64_encode(image)
-#print(base64) #/9j/4AAQSkZJRgABAQAAA ... bSjrTEf/9k=
-def base64_encode(image):
-    bytesIO = io.BytesIO()
-    try:
-        image.save(bytesIO, "JPEG")
-    except:
-        image.save(bytesIO, "PNG")
-    b64encoded = base64.b64encode(bytesIO.getvalue())
-    base64_str = b64encoded.decode("utf-8")
-    return base64_str
-'''
+import numpy
 
 '''
 image = Image.open("rock.jpg")
@@ -28,19 +14,17 @@ print(base64) #data:image/png;base64,/9j/4AAQSkZJRgABAQ...2qjR37P/2Q==
 base64 = base64_encode(audio)
 print(base64) #data:audio/wav;base64,UklGRiTuAgBXQVZFZm...At84WACNZGwA=
 '''
-def base64_encode(bytes):
-    if isinstance(bytes, Image):
+def base64_encode(image):
+    if isinstance(image, Image.Image):
         bytes_io = io.BytesIO()
-        #try:
-        #    image.save(bytes_io, "JPEG")
-        #except:
-        #    image.save(bytes_io, "PNG")
-        bytes.save(bytes_io, bytes.format)
+        image.save(bytes_io, image.format)
         bytes = bytes_io.getvalue()
+    elif isinstance(image, numpy.ndarray):
+        bytes = image
     base64_encoded = base64.b64encode(bytes)
-    base64_encoded = base64_encoded.decode("utf-8")
-    if isinstance(bytes, Image):    
+    base64_encoded = base64_encoded.decode("utf-8") 
+    if isinstance(image, Image.Image):
         #return "data:image/png;base64,"+base64_encoded
-        return "data:image/"+bytes.format.lower()+";base64,"+base64_encoded
-    else:    
+        return "data:image/"+image.format.lower()+";base64,"+base64_encoded
+    elif isinstance(image, numpy.ndarray): 
         return "data:audio/wav;base64,"+base64_encoded
