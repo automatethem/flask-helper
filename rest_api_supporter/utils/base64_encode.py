9ba550850c74f89b8c4f6ae5120bedb7d018700e
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-01 04:23:13 +0900

    Update base64_encode.py

--- rest_api_supporter/utils/base64_encode.py
+++ rest_api_supporter/utils/base64_encode.py
@@ -14,26 +14,16 @@ def base64_encode(image):
             image_format = "PNG"
         image.save(bytes_io, image_format)
         bytes_value = bytes_io.getvalue()
-    elif isinstance(image, numpy.ndarray):
-        '''
-        with open("up.wav", "rb") as f:
-            bytes_value = f.read()
-        '''
-        #'''
-        wav_file = f"speech_{random.randint(1, 10000)}.wav"
-        try:
-            sf.write(wav_file, image, samplerate=16000)
-            with open(wav_file, "rb") as f:
-                bytes_value = f.read()
-        finally:
-            os.remove(wav_file)
-        #'''
-    elif isinstance(image, bytes):
-        bytes_value = image
-    base64_encoded = base64.b64encode(bytes_value)
-    base64_encoded = base64_encoded.decode("utf-8") 
-    if isinstance(image, Image.Image):
+
+        base64_encoded = base64.b64encode(bytes_value)
+        base64_encoded = base64_encoded.decode("utf-8") 
+
         #return "data:image/png;base64,"+base64_encoded
         return "data:image/"+image_format.lower()+";base64,"+base64_encoded
-    elif isinstance(image, numpy.ndarray) or isinstance(image, bytes): 
+    elif isinstance(image, bytes):
+        bytes_value = image
+
+        base64_encoded = base64.b64encode(bytes_value)
+        base64_encoded = base64_encoded.decode("utf-8") 
+     
         return "data:audio/wav;base64,"+base64_encoded
