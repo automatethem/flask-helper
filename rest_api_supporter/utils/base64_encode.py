658c27a6cbf0755222f4d56d8249f30667603bce
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-03 06:13:56 +0900

    Update base64_encode.py

--- rest_api_supporter/utils/base64_encode.py
+++ rest_api_supporter/utils/base64_encode.py
@@ -1,7 +1,7 @@
 import io
 import base64
 from PIL import Image
-import numpy
+import numpy as np
 import random
 import soundfile as sf
 import os
@@ -20,6 +20,16 @@ def base64_encode(image):
 
         #return "data:image/png;base64,"+base64_encoded
         return "data:image/"+image_format.lower()+";base64,"+base64_encoded
+    elif isinstance(image, np.array): #오디오
+        numpy_array = image
+        file = "audio.wav"
+        sf.write(file, numpy_array, samplerate=16000)
+        with open(file, "rb") as f:
+            bytes_value = f.read() #bytes
+        base64_encoded = base64.b64encode(bytes_value)
+        base64_encoded = base64_encoded.decode("utf-8") 
+     
+        return "data:audio/wav;base64,"+base64_encoded
     elif isinstance(image, bytes): #오디오
         bytes_value = image
 
