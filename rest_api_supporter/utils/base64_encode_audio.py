66acbfd465534382e9ff336b24064e54b578360d
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-23 12:35:21 +0900

    Update base64_encode_audio.py

--- rest_api_supporter/utils/base64_encode_audio.py
+++ rest_api_supporter/utils/base64_encode_audio.py
@@ -1,22 +1,24 @@
 import io
 import base64
-from PIL import Image
 import numpy as np
-import random
 import soundfile as sf
 import os
 
-def base64_encode_audio(image):
-    if isinstance(image, np.ndarray): #오디오
-        numpy_array = image
+def base64_encode_audio(audio):
+    if isinstance(audio, np.ndarray): 
         file = "audio.wav"
         try:
-            sf.write(file, numpy_array, samplerate=16000)
+            sf.write(file, audio, samplerate=16000)
             with open(file, "rb") as f:
                 bytes_value = f.read() #bytes
         finally:
             os.remove(file)
         base64_encoded = base64.b64encode(bytes_value)
+        base64_encoded = base64_encoded.decode("utf-8") 
+
+        return "data:audio/wav;base64,"+base64_encoded
+    elif isinstance(audio, bytes): 
+        base64_encoded = base64.b64encode(audio)
         base64_encoded = base64_encoded.decode("utf-8") 
      
         return "data:audio/wav;base64,"+base64_encoded
