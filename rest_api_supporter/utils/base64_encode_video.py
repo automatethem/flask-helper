7cde772362c131dcabdf26c23c42cd91f651b562
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-23 13:06:33 +0900

    Update base64_encode_video.py

--- rest_api_supporter/utils/base64_encode_video.py
+++ rest_api_supporter/utils/base64_encode_video.py
@@ -6,7 +6,7 @@ def base64_encode_video(video):
         base64_encoded = base64.b64encode(video)
         base64_encoded = base64_encoded.decode("utf-8") 
         return "data:video/mp4;base64,"+base64_encoded
-    elif isinstance(video, np.array):
+    elif isinstance(video, np.ndarray):
         bytes_value = video.tobytes()
         base64_encoded = base64.b64encode(bytes_value)
         base64_encoded = base64_encoded.decode("utf-8") 
