73c4b7a73588c49705d1a04f01f66dd47061b838
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-23 12:31:25 +0900

    Update base64_encode_image.py

--- rest_api_supporter/utils/base64_encode_image.py
+++ rest_api_supporter/utils/base64_encode_image.py
@@ -6,7 +6,7 @@ import random
 import soundfile as sf
 import os
 
-def base64_encode(image):
+def base64_encode_image(image):
     if isinstance(image, Image.Image): #이미지
         #'''
         bytes_io = io.BytesIO()
@@ -29,23 +29,4 @@ def base64_encode(image):
         base64_encoded = base64_encoded.decode("utf-8") 
         #return "data:image/png;base64,"+base64_encoded
         return "data:image/"+image_format.lower()+";base64,"+base64_encoded
-    elif isinstance(image, np.ndarray): #오디오
-        numpy_array = image
-        file = "audio.wav"
-        try:
-            sf.write(file, numpy_array, samplerate=16000)
-            with open(file, "rb") as f:
-                bytes_value = f.read() #bytes
-        finally:
-            os.remove(file)
-        base64_encoded = base64.b64encode(bytes_value)
-        base64_encoded = base64_encoded.decode("utf-8") 
-     
-        return "data:audio/wav;base64,"+base64_encoded
-    elif isinstance(image, bytes): #비디오
-        bytes_value = image
-
-        base64_encoded = base64.b64encode(bytes_value)
-        base64_encoded = base64_encoded.decode("utf-8") 
-     
-        return "data:video/mp4;base64,"+base64_encoded
+    
