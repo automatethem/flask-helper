7772900756c97de90a044f54de7c5f5855d4e0ef
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-23 19:52:18 +0900

    Update base64_decode_video.py

--- rest_api_supporter/utils/base64_decode_video.py
+++ rest_api_supporter/utils/base64_decode_video.py
@@ -14,9 +14,9 @@ def base64_decode_video(base64_encoded, to="numpy"):
     #print(base64_encoded) #UklGRiTuAgBXQVZFZm...At84WACNZGwA=
     base64_decoded = base64.b64decode(base64_encoded) #bytes
 
-    if to = "bytes":
+    if to == "bytes":
         return base64_decoded
-    elif to = "numpy":
+    elif to == "numpy":
         base64_decoded = np.frombuffer(base64_decoded, np.uint8) #numpy array
         #video = cv2.imdecode(base64_decoded, cv2.IMREAD_UNCHANGED)
         #cv2.imwrite("result.mp4", video)
