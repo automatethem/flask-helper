95ae53dae2bc0c4638e580b6ba9df6308dc66764
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-25 14:42:35 +0900

    Update base64_encode_audio.py

--- rest_api_supporter/utils/base64_encode_audio.py
+++ rest_api_supporter/utils/base64_encode_audio.py
@@ -20,4 +20,9 @@ def base64_encode_audio(audio):
         base64_encoded = base64.b64encode(bytes_value)
         base64_encoded = base64_encoded.decode("utf-8") 
         return "data:audio/wav;base64,"+base64_encoded
-
+    elif isinstance(audio, str): 
+        with open(audio, "rb") as f:
+            bytes_value = f.read() #bytes
+        base64_encoded = base64.b64encode(bytes_value)
+        base64_encoded = base64_encoded.decode("utf-8") 
+        return "data:audio/wav;base64,"+base64_encoded
